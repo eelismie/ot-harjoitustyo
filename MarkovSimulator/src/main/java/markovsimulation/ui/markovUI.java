b63a629a838d8e917fd613c137a42f87271432b0
volatilequark <evmielonen@gmail.com>
2019-03-29 16:28:57 +0200

    simple ui elements

---
+++ MarkovSimulator/src/main/java/markovsimulation/ui/markovUI.java
@@ -0,0 +1,92 @@
+package markovsimulation.ui;
+
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class markovUI extends Application {
+    
+    @Override
+    public void start(Stage window){
+        window.setHeight(300);
+        window.setWidth(500);
+        window.setTitle("Markov Process Simulation");
+        
+        //___load Scene___
+                
+        //buttonfield - Bottom
+        HBox buttonfield1 = new HBox();
+        buttonfield1.setPadding(new Insets(10));
+        Button next1 = new Button("next");
+        Button back1 = new Button("quit");
+        Region spacer = new Region();
+        HBox.setHgrow(spacer, Priority.ALWAYS);
+        buttonfield1.getChildren().setAll(back1, spacer, next1);
+        
+        //inputfield - Center
+        VBox inputfield1 = new VBox();
+        inputfield1.setAlignment(Pos.CENTER);
+        inputfield1.setPadding(new Insets(10));
+        
+        HBox textinput1 = new HBox();
+        textinput1.setSpacing(5);
+        Button loadbutton = new Button("load");
+        TextField filelocation = new TextField();
+        Label loadlabel = new Label("Simulation file location: ");
+        textinput1.getChildren().setAll(loadlabel, filelocation, loadbutton);
+        inputfield1.getChildren().setAll(textinput1);
+        
+        //Scene Root
+        BorderPane frame1 = new BorderPane();
+        frame1.setBottom(buttonfield1);
+        frame1.setCenter(inputfield1);
+        
+        Scene loadscene = new Scene(frame1);
+        
+        //___edit scene___
+        
+        // Buttonfield - Bottom
+        HBox buttonfield2 = new HBox();
+        buttonfield2.setPadding(new Insets(10));
+        Button next2 = new Button("run");
+        Button back2 = new Button("back");
+        Region spacer2 = new Region();
+        HBox.setHgrow(spacer2, Priority.ALWAYS);
+        buttonfield2.getChildren().setAll(back2, spacer2, next2);
+        
+        BorderPane frame2 = new BorderPane();
+        frame2.setBottom(buttonfield2);
+        
+        Scene editscene = new Scene(frame2);
+        
+        //___result scene___
+        
+        //___scene-switch button functionality
+        
+        next1.setOnAction(e -> {
+            window.setScene(editscene);
+        });
+        
+        back2.setOnAction(e -> {
+            window.setScene(loadscene);
+        });
+        
+        window.setScene(loadscene);
+        window.show();
+    }
+    
+    public static void main(String[] args){
+        launch(args);
+    }
+}
