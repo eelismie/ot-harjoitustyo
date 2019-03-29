b055bac987f55b4a8be39656c3b0096bd53eb973
volatilequark <evmielonen@gmail.com>
2019-03-29 16:47:49 +0200

    additions to manager

--- MarkovSimulator/src/main/java/markovsimulation/domain/markovManager.java
+++ MarkovSimulator/src/main/java/markovsimulation/domain/markovManager.java
@@ -4,8 +4,23 @@
  * and open the template in the editor.
  */
 package markovsimulation.domain;
+import java.util.ArrayList;
+import java.util.HashSet;
 
 public class markovManager {
+    boolean statesaved;
+    boolean simexists;
+    HashSet<String> names;
+    ArrayList<String> namenumbers;
+    int nodes;
     
+    public markovManager(){
+        statesaved = false;
+        simexists = false;
+        namenumbers = new ArrayList<>();
+    }
     
+    public void addNode(String description){
+        
+    }
 }
