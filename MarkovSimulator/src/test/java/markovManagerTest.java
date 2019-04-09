13dd69c7f5f04551f3acd268c0f7f188e3bfb7ed
volatilequark <evmielonen@gmail.com>
2019-04-09 22:10:14 +0300

    tests added

--- MarkovSimulator/src/test/java/markovManagerTest.java
+++ MarkovSimulator/src/test/java/markovManagerTest.java
@@ -5,6 +5,7 @@
  */
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -27,13 +28,43 @@ public class markovManagerTest {
     
     @Test
     public void addNodeOk() {
-        manager.addNode("   event1");
+        manager.addNode("event1");
         manager.addNode("event2 ");
         manager.addNode("event3");
-        
         ArrayList<String> a = new ArrayList<>();
         a.add("event1");
         a.add("event2");
-        a.add("event3");
+        a.add("event3");        
+        assertTrue(a.equals(manager.getSimDescription().getNodes()));
+        assertTrue(manager.getSimDescription().getConnects().size()==3);
+    }
+    
+    @Test
+    public void addBadConnectOk() {
+        assertTrue(!manager.addConnect("0", "5"));
+        assertTrue(!manager.addConnect("b", "e"));
+        assertTrue(!manager.addConnect("", "2"));
+    }
+    
+    public void simulationCycleWorks() {
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("event1 : 0.0");
+        expected.add("event2 : 0.5");
+        expected.add("event3 : 0.5");
+        ArrayList<String> expected2 = new ArrayList<>();
+        expected.add("event1 : 0.0");
+        expected.add("event2 : 1.0");
+        expected.add("event3 : 0.0");
+        
+        manager.addNode("event1");
+        manager.addNode("event2");
+        manager.addNode("event3");
+        manager.addConnect("0", "1");
+        manager.addConnect("0", "2");
+        manager.initsim();
+        manager.evolveCurrentSim(1);
+        assertTrue(expected.equals(manager.getProbabilities()));
+        manager.setResultDisplay(1);
+        assertTrue(expected2.equals(manager.getProbabilities()));
     }
 }
